0ba2c833ddb58b2e8bb6b34a35d926d9f5c2d9d3
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-02-06 19:59:45 +0100

    New hash

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const ADMIN_URL = 'ws://localhost:22222';
 export const APP_URL = 'ws://localhost:22223';
 export const COMPOSITORY_DNA_HASH =
-'uhC0kwJ9t0UOFe-u3GuDUHfm5aGinLupUYQNMBdomLkmDznH0I2Wc';
+'uhC0klH1NAzmRGd-AKfJ6bw_yWlBxyJITwrzMqZOJ3dSKv4beYKW-';
 export const DOCKER_DESTKOP_URL = 'https://www.docker.com/products/docker-desktop';
