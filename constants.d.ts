cbf1f647e0e75efc79962725a3c1185fad737149
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-02-06 21:07:40 +0100

    Updates

--- constants.d.ts
+++ constants.d.ts
@@ -1,4 +1,4 @@
 export declare const ADMIN_URL = "ws://localhost:12222";
 export declare const APP_URL = "ws://localhost:12223";
-export declare const COMPOSITORY_DNA_HASH = "uhC0kwJ9t0UOFe-u3GuDUHfm5aGinLupUYQNMBdomLkmDznH0I2Wc";
+export declare const COMPOSITORY_DNA_HASH = "uhC0klH1NAzmRGd-AKfJ6bw_yWlBxyJITwrzMqZOJ3dSKv4beYKW-";
 export declare const DOCKER_DESTKOP_URL = "https://www.docker.com/products/docker-desktop";
