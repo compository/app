20dbb5cd0367c9e25b5659681578056efbe3346e
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-03-18 16:35:44 +0100

    Updated dna hash

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const ADMIN_URL = 'ws://localhost:22222';
 export const APP_URL = 'ws://localhost:22223';
 export const COMPOSITORY_DNA_HASH =
-  'uhC0kFz1Nk6gCq-V2VKMwGvQndD15l2vuzhOu7XSW701VP80F13Vp';
+  'uhC0kmRDhpGo-CQUvldcIigA4kMwuMpqA2viDKkYq-ENIuJg93O3j';
 export const DOCKER_DESTKOP_URL =
   'https://www.docker.com/products/docker-desktop';
